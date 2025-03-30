ea784ea1fbba3ab8645b8912421481aad7ecef8a
Nicolas <nicocuabrango1230@gmail.com>
2025-03-30 02:26:40 -0500

    todo listo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { MakeupStock } from './makeup-stock/makeup-stock.entity';
-import { ProductTest } from './product-tests/product-test.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { MakeupStockModule } from './makeup-stock/makeup-stock.module';
 import { ProductTestsModule } from './product-tests/product-tests.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
+  
+  imports: [MakeupStockModule, ProductTestsModule, OrdersModule, ConfigModule.forRoot(),//Me permite usar variables de entorno
+    TypeOrmModule.forRoot({//Configuración de la base de datos
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
-    }),
-    TypeOrmModule.forFeature([MakeupStock, ProductTest]),
-    MakeupStockModule,
-    ProductTestsModule,
-  ],
+      synchronize: true, 
+    }), UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
